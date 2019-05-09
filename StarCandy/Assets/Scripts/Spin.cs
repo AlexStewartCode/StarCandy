10982eabeda5fa5d4c23d1f0e8d9faaee70e399c
Alex <github@alexstewart.app>
2019-05-09 00:35:35 -0400

    + Added spin demo

---
+++ StarCandy/Assets/Scripts/Spin.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct RotationInfo
+{
+    public float XSpin;
+    public float YSpin;
+}
+
+public class Spin : MonoBehaviour
+{
+    public RotationInfo rotationInfo;
+
+    private void Awake()
+    {
+        Debug.Log("Awake!");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //var rotation = this.gameObject.transform.rotation;
+        this.gameObject.transform.Rotate(Vector3.up, rotationInfo.YSpin);
+        this.gameObject.transform.Rotate(Vector3.right, rotationInfo.XSpin);
+    }
+}
