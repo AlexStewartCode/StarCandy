1533822c6008efd582a4fca761e69a8d6ffd9125
Matthew Cech <contact@matthewcech.com>
2019-05-17 00:34:58 -0700

    + Added startup menu logo

---
+++ StarCandy/Assets/Scripts/UI/UIOscillate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceCandy
+{
+    [RequireComponent(typeof(RectTransform))]
+    public class UIOscillate : MonoBehaviour
+    {
+        public float distance = 10;
+        public float speed = 1;
+
+        private Vector3 pos;
+
+        private void Start()
+        {
+            pos = (transform as RectTransform).position;
+        }
+
+        private void Update()
+        {
+            RectTransform rt = this.transform as RectTransform;
+            rt.position = pos + new Vector3(0, Mathf.Sin(Time.realtimeSinceStartup * speed) * distance, 0);
+        }
+    }
+}
